aee4a5fd0bbd916490ec70b7c83b7cd459ce9f72
Gurjinder <70706370+GakhalSingh@users.noreply.github.com>
2021-12-21 21:29:58 +0100

    Undo Error

--- Templates/Defaults/head.php
+++ Templates/Defaults/head.php
@@ -2,9 +2,8 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title><?= getTitle() ?></title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="/css/style.css"/>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap.bundle.js"></script>
 </head>
